f780b512709224025ad99f55968d68af14059309
Dunia Dunia <ddunia680@gmail.com>
2022-12-28 16:04:43 +0300

    routing added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './header/header';
 import Section1 from './Section1/section1';
 import Section2 from './section2/section2';
 import Footer from './Footer/footer';
 
-function App() {
-  return (
-    <div>
-      <Header/>
+const home = (
+  <div>
       <Section1/>
       <Section2/>
       <Footer/>
     </div>
+)
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/' element={ <Header/> }>
+          <Route path='home' element={ home }/>
+          <Route path='/movie' element={<h1 className='pt-40'>Welcome to this side</h1>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/header/header.js
+++ src/header/header.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faLaptop, faSearch, faUserGroup, faVideo } from '@fortawesome/free-solid-svg-icons'
 
 import './header.css';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function header(props) {
 
@@ -12,39 +13,42 @@ function header(props) {
     //     navDarker = 'bg-waydarkerBlur';
     // })
     return (
-        <div>
-            <nav className='header w-screen h-7 mx-auto px-0 bg-waydarkerBlur flex flex-row justify-between px-2 lg:justify-around lg:h-20 lg:px-6'>
-          {/* Left side */}
-          <div className='flex flex-row items-center justify-between px-0 lg:px-5'>
-            <h1 className='hidden font-bold text-2xl text-white pr-10 md:block'>betaseries</h1>
-            <div className='flex flex-row justify-start items-center space-x-4 lg:space-x-6 text-white'>
-                <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><FontAwesomeIcon icon={faHome} />
-                  <span className='hidden lg:block'>Home</span>
-                </p>
-                <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><FontAwesomeIcon icon={faLaptop} />
-                  <span className='hidden lg:block'>Shows</span>
-                </p>
-                <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><FontAwesomeIcon icon={faVideo} />
-                  <span className='hidden lg:block'>Movies</span>
-                </p>
-                <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><FontAwesomeIcon icon={faUserGroup} />
-                  <span className='hidden lg:block'>Community</span>
-                </p>
-            </div>
-            
-          </div>
-          {/* Right side */}
-          <div className='flex flex-row justify-start items-center space-x-5'>
-            <button className='  hidden signUp text-white px-3 py-1 border-none lg:block  hover:bg-black hover:text-white-500'>Sign up</button>
-            <p className=' text-chocolateBrayish text-sm lg:text-md hover:text-white'>Sign in</p>
-            <span className=' text-chocolateBrayish text-sm lg:text-md hover:text-white'>
-              <FontAwesomeIcon icon={faSearch} />
-            </span>
-          </div>
+      <>
+        <header className='fixed top-0 left-0'>
+            <nav className='w-screen h-7 mx-auto bg-waydarkerBlur flex flex-row justify-between px-2 lg:justify-around lg:h-20 lg:px-6'>
+              {/* Left side */}
+              <div className='flex flex-row items-center justify-between px-0 lg:px-5'>
+                <h1 className='hidden font-bold text-2xl text-white pr-10 md:block'>betaseries</h1>
+                <div className='flex flex-row justify-start items-center space-x-4 lg:space-x-6 text-white'>
+                    <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><NavLink to="/home" className='flex flex-row items-center space-x-2'><FontAwesomeIcon icon={faHome} />
+                      <span className='hidden lg:block'>Home</span></NavLink>
+                    </p>
+                    <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><NavLink to="/movie" className='flex flex-row items-center space-x-2'><FontAwesomeIcon icon={faLaptop} />
+                      <span className='hidden lg:block'>Shows</span></NavLink>
+                    </p>
+                    <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><NavLink to="/" className='flex flex-row items-center space-x-2'><FontAwesomeIcon icon={faVideo} />
+                      <span className='hidden lg:block'>Movies</span></NavLink>
+                    </p>
+                    <p className=' flex flex-row items-center space-x-2 text-chocolateBrayish text-sm lg:text-md hover:text-white hover:cursor-pointer'><NavLink to="/" className='flex flex-row items-center space-x-2'><FontAwesomeIcon icon={faUserGroup} />
+                      <span className='hidden lg:block'>Community</span></NavLink>
+                    </p>
+                </div>
+              
+              </div>
+              {/* Right side */}
+              <div className='flex flex-row justify-start items-center space-x-5'>
+                <button className='  hidden signUp text-white px-3 py-1 border-none lg:block  hover:bg-black hover:text-white-500'>Sign up</button>
+                <p className=' text-chocolateBrayish text-sm lg:text-md hover:text-white'>Sign in</p>
+                <span className=' text-chocolateBrayish text-sm lg:text-md hover:text-white'>
+                <FontAwesomeIcon icon={faSearch} />
+                </span>
+              </div>
         
-      </nav>
+          </nav>
             
-        </div>
+        </header>
+        <Outlet/>
+      </>
     );
 }
 
